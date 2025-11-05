cfbdc02bf84d19d2c038e7547c05d07f7c04ee47
Nivrutti Tidke <nivrutitidke108@gmail.com>
2025-11-05 20:08:41 +0530

    Final session fix - working on Render

--- app.js
+++ app.js
@@ -53,15 +53,20 @@ store.on("error",(err)=>{
     console.log("ERROR IN MONGO SESSION STORE",err);
 });
 
+app.set("trust proxy", 1);
+
 const sessionOption = {
     store,
     secret:process.env.SECRET,
     resave:false,
-    saveUninitialized:true,
+    saveUninitialized:false,
     cookie: {
+        httpOnly: true,
+       secure: true,          
+       sameSite: "none",
         expires: Date.now() + 7 *24 *60 *60 *100,
         maxAge: 7 * 24 * 60 * 60 *100,
-        httpOnly: true,
+       
     },
 };
 
