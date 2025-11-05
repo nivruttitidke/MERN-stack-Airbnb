7b46d521b6f7494b912ea466ad7c4fb8fa8220c2
Nivrutti Tidke <nivrutitidke108@gmail.com>
2025-11-05 19:18:13 +0530

    Fix home route redirect

--- app.js
+++ app.js
@@ -81,6 +81,9 @@ app.use((req,res,next)=>{
     res.locals.currUser = req.user;
     next();
 });
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use("/listings",listingRouter);
 app.use("/listings",reviewRouter);
